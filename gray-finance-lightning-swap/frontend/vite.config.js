35801c79952669e96c9b2c64a7283152ce324b01
leffw <93302536+leffw@users.noreply.github.com>
2022-11-03 02:26:26 -0300

    Update vite.config.js

--- gray-finance-lightning-swap/frontend/vite.config.js
+++ gray-finance-lightning-swap/frontend/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173
   },
+  preview: {
+    host: "0.0.0.0",
+    port: 5173
+  },
   build: {
     minify: false,
     sourcemap: false,
