ccf6f11e84bf744b469b7909bd06a7dbc7a98037
leffw <93302536+leffw@users.noreply.github.com>
2022-11-16 17:42:44 -0300

    Update docker-entrypoint.sh

--- gray-finance-lightning-swap/frontend/docker-entrypoint.sh
+++ gray-finance-lightning-swap/frontend/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ ! -d "dist" ]; then
-    npm run build
-else
-    npm i @types/react @types/react-dom @vitejs/plugin-react vite cross-env
-fi
+    npm i
+    npm run build    
+fi 
